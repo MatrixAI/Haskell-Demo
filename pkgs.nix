c4288b436cfecb69cdbcb5625839a24400306e24
Roger Qiu <roger.qiu@polyhack.io>
2020-01-03 18:21:42 +1100

    Updated to b0bbacb52134a7e731e549f4c0a7a2a39ca6b481

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (
-  let rev = "d85e435b7bded2596d7b201bcd938c94d8a921c1"; in
+  let rev = "b0bbacb52134a7e731e549f4c0a7a2a39ca6b481"; in
   fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/${rev}.tar.gz"
 ) {}
