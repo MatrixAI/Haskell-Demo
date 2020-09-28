ec7beb4ffdf16e3d37c75b872a2563eb7cfee815
Ning Zhang <ning.zhang@matrix.ai>
2020-09-28 16:20:15 +1000

    Add static executable example

--- release.nix
+++ release.nix
@@ -10,6 +10,9 @@ in
   rec {
     library = drv;
     application = drv;
+    # justStaticExecutables applies static linking to haskell libraries. 
+    # System libraries are still dynamically linked.
+    applicationStatic = pkgs.haskell.lib.justStaticExecutables drv;
     docker = dockerTools.buildImage {
       name = application.name;
       contents = application;
